f934d8167e7f799ca884c7fcea2e72754d071223
Manas Ghai <manasg46@gmail.com>
2022-02-17 22:27:22 +0530

    initial commit

---
+++ hello_world.js
@@ -0,0 +1,21 @@
+var number = 5; // inline comment
+
+console.log("Hello World!")
+/* this
+is 
+a 
+mulitiline
+comment
+*/
+
+/* Data Types:
+undefined, null, boolean, number, string, symbol and object
+*/
+
+  var myName = "Manas"
+
+  myName = "Manas Ghai"
+
+  let ourName = "github"
+
+  const pi = 3.14
